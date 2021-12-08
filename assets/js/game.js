8b3c1b0fac508d4fb4c0bd692cb3a2e34d4fb810
Rochelle Younger <regnuoy321@gmail.com>
2021-12-08 09:58:16 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,35 +1,15 @@
-/* var playerName = window.prompt("What is your robot's name?");
-
-console.log(playerName);
-
-console.log("This logs a string, good for leaving yourself a message");
-
-//this will do mth and log 20
-console.log(10 + 10);
-
-//what is this?
-console.log("Our robot's name is " + playerName);
-
-// this creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
-}
-
-fight(); */
-
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+// var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android","Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -89,4 +69,58 @@ var fight = function() {
     }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+
+// fight();
+
+
+
+
+// EXTRA CODE FOR LEARNING/TESTING PURPOSES
+
+
+// You can also log multiple values at once like this
+// console.log(playerName, playerAttack, playerHealth);
+
+/* var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+//this will do mth and log 20
+console.log(10 + 10);
+
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+fight(); */
+
+
+// console.log(enemyNames);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+// console.log(enemyNames[3]);
+// console.log(enemyNames.length);
+// for(var i=0; i < 3; i++) {
+//     console.log("apple", i);
+// }
+// for(var i = 0; i < enemyNames.length; i++) {
+//     console.log(enemyNames[i]);
+//     console.log(i);
+//     console.log(enemyNames[i] + " is at " + i + " index");
+// }
+
+// var test = function(word) {
+//     console.log("I love " + word);
+// }
+
+// test(playerName);
