fdf7ec781b4c90bb8d307cc9cf31e1391fe5468e
Rochelle Younger <regnuoy321@gmail.com>
2021-12-10 10:32:08 -0500

    fixed player-name prompt bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,28 @@
 // function to generate a random numeric value
-var randomNumber = function() {
-    var value = Math.floor(Math.random() * 21) + 40;
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
     
     return value;
 }
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+
+    // ***************************************
+    // ADD LOOP HERE WITH PROMPT AND CONDITION
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    };
+    // ***************************************
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    // name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -132,7 +148,7 @@ var fight = function(enemy) {
 
             // leave while() loop since enemy is dead
             break;
-        }else {
+        } else {
             window.alert(enemy.name + " still has " + enemy.health + " health left.");
         }
         
@@ -192,7 +208,7 @@ var startGame = function() {
                     shop();
                 }
             }
-        }else {
+        } else {
             window.alert("You have lost your robot in battle! Game Over!");
             break;
         }
